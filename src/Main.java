56e72b4917c57c739ac0e3ad406384cbacf00283
Mikhail Bukhtoyarov <m.buhtoyarov@yandex.ru>
2023-02-20 23:31:34 +0300

    solution v3

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,37 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        List<String> directories = new ArrayList<>();
+        directories.addAll(Arrays.asList("D://Games/res", "D://Games/src", "D://Games/savegames", "D://Games/temp",
+                "D://Games/src/main", "D://Games/src/test", "D://Games/src/main/", "D://Games/res/drawables",
+                "D://Games/res/vectors", "D://Games/res/icons"));
+
+        for (String i : directories) {
+            newFolder(i);
+        }
+
+        newFile("D://Games/temp/", "temp.txt");
+        newFile("D://Games/src/main/", "Main.java");
+        newFile("D://Games/src/main/", "Utils.java");
+
+        try (FileWriter writer = new FileWriter("D://Games/temp/temp.txt")) {
+            writer.write(String.valueOf(sb));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        List<String> list = Arrays.asList("D://Games/savegames/save.dat", "D://Games/savegames/save1.dat", "D://Games/savegames/save2.dat");
+
+        saveGame(list.get(0), new GameProgress(66, 55, 2, 2.2));
+        saveGame(list.get(1), new GameProgress(99, 99, 100, 152.5));
+        saveGame(list.get(2), new GameProgress(21, 2, 1, 0.1));
+
+        zipFiles("D://Games/savegames/zip.zip", list);
+    }
 
     static void saveGame(String pathSaveFile, GameProgress gameProgress) {
         try (FileOutputStream fos = new FileOutputStream(pathSaveFile);
@@ -18,21 +49,23 @@ public class Main {
 
     }
 
-    static String newFolder(String pathFolder) {
+    static void newFolder(String pathFolder) {
         File dir = new File(pathFolder);
         if (dir.mkdir()) {
-            return LocalDateTime.now() + " Папка " + dir.getName() + " создана" + "\n";
+//            return LocalDateTime.now() + " Папка " + dir.getName() + " создана" + "\n";
+            sb.append(LocalDateTime.now() + " Папка " + pathFolder + " успешно создана \n");
         } else
-            return LocalDateTime.now() + " Папка " + dir.getName() + " не создана" + "\n";
+//            return LocalDateTime.now() + " Папка " + dir.getName() + " не создана" + "\n";
+            sb.append(LocalDateTime.now() + " Папка " + pathFolder + " не создана \n");
     }
 
-    static String newFile(String pathFile, String nameFile) {
+    static void newFile(String pathFile, String nameFile) {
         File file = new File(pathFile + nameFile);
         try {
             if (file.createNewFile()) {
-                return LocalDateTime.now() + " Файл " + file.getName() + " создан" + "\n";
+                sb.append(LocalDateTime.now() + " Файл " + nameFile + " Успешно создан \n");
             } else
-                return LocalDateTime.now() + " Файл " + file.getName() + " не создан" + "\n";
+                sb.append(LocalDateTime.now() + " Файл " + nameFile + " не создан \n");
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -61,41 +94,7 @@ public class Main {
 
     }
 
-    public static void main(String[] args) {
 
-        List<String> directories = new ArrayList<>();
-        directories.addAll(Arrays.asList("D://Games/res", "D://Games/src", "D://Games/savegames", "D://Games/temp",
-                "D://Games/src/main", "D://Games/src/test", "D://Games/src/main/", "D://Games/res/drawables",
-                "D://Games/res/vectors", "D://Games/res/icons"));
-
-        StringBuilder sb = new StringBuilder();
-
-        for (String i : directories) {
-            sb.append(newFolder(i));
-        }
-
-        sb.append(newFile("D://Games/temp/", "temp.txt"));
-        sb.append(newFile("D://Games/src/main/", "Main.java"));
-        sb.append(newFile("D://Games/src/main/", "Utils.java"));
-
-        try (FileWriter writer = new FileWriter("D://Games/temp/temp.txt")) {
-            writer.write(String.valueOf(sb));
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        GameProgress gameProgress1 = new GameProgress(66, 55, 2, 2.2);
-        GameProgress gameProgress2 = new GameProgress(99, 99, 100, 152.5);
-        GameProgress gameProgress3 = new GameProgress(21, 2, 1, 0.1);
-
-        List<String> list = Arrays.asList("D://Games/savegames/save.dat", "D://Games/savegames/save1.dat", "D://Games/savegames/save2.dat");
-
-        saveGame(list.get(0), gameProgress1);
-        saveGame(list.get(1), gameProgress2);
-        saveGame(list.get(2), gameProgress3);
-
-        zipFiles("D://Games/savegames/zip.zip", list);
-    }
 }
 
 
